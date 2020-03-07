01c5a3fdb0660eda554680ad814b440c2715a899
liu_home <lys_uestc1@163.com>
2020-03-07 22:21:07 +0800

    将timeout设置为大的值，处理好断线问题

--- download.py
+++ download.py
@@ -9,6 +9,7 @@ import io
 
 #TODO:  断点重连部分需要解决。现在下载还是不完整
 #TODO:  使用beautiful soup或者lxml 来优雅地处理html数据。 
+#NOTE: 使用前需要修改的是 reg， url 和downprocess里的url
 
 def download_file(url,path,filename):
     
@@ -53,39 +54,45 @@ def find_url(html, keyword, reg):
         print("没有找到关键词，下载失败")
         return []
 
-def download_process(down_url,keyword):
+def download_process(down_url,keyword,path):
     #cs161 url = 'http://web.stanford.edu/class/cs161/'
     #cs 166 https://web.stanford.edu/class/archive/cs/cs166/cs166.1146/lectures/00/Slides00.pdf
-    url = 'https://web.stanford.edu/class/archive/cs/cs166/cs166.1146/'
-    url = url + down_url
+    url = r'https://15445.courses.cs.cmu.edu/fall2019'
+    url = url+down_url
+    print(url)
     filename =  down_url.split('/')[-1]
+    print (filename)
     print('正在下载' + filename + '个文件，图片地址:' + path+filename)
     download_file(url,path,filename)
 
 
 
-    
-
-
 #cs 161 下载地址 http://web.stanford.edu/class/cs161/Lectures/Lecture1/Lecture1.pptx
 #cs  166 下载地址 https://web.stanford.edu/class/archive/cs/cs166/cs166.1146/lectures/00/Slides00.pdf
 if __name__ == '__main__':
-    keyword = input("Input file type: ")
-    url = input("Input url:")
-    reg = input("Input regex:")
-     
+    # keyword = input("Input file type: ")
+    # url = input("Input url:")#http://www-inst.eecs.berkeley.edu/~cs61a/fa11/61a-python/content/www/index.html
+    # reg = input("Input regex:")
+    
+    keyword="pdf"
+    url= r'https://15445.courses.cs.cmu.edu/fall2019/schedule.html'
+    reg = '(?<=href="\.).{1,30}\.pdf(?=")'  
+
     result = requests.get(url)
     down_url = find_url(result.text.encode('ISO-8859-1').decode('utf-8'), keyword,reg) 
     #  查看resul.encode 来查看编码方式。填入相应的编码解码方式。
-    #获取下载地址
+    #获取下载地址ca
     path_name = input('储存文件夹名字')
     path = os.path.join(os.getcwd()+os.sep+path_name+os.sep+keyword+os.sep)
     if down_url != []:
-    #多进程
+        #单进程
+        # for i in down_url:
+        #     download_process(i,keyword,path)
+        #多进程
         p = Pool(5)
         print(down_url)
         for i in down_url:
-            p.apply_async(download_process,args=(i,keyword,))
+            p.apply_async(download_process,args=(i,keyword,path))
         print('Waiting for all subprocesses done...')
         p.close()
         p.join()
--- readme.md
+++ readme.md
@@ -1,7 +1,22 @@
 #示例
     cs161 reg = '(?<=a href=").*Slides\d*[^/]*.pdf(?=">)'
     cs166 reg = '(?<=<li><a href=").{1,20}/Slides\d*.pdf(?=">)'
+    cs61A reg = '(?<=a href=")slides/.{1,25}\.(pdf|py)(?=">)'
+    cmu network reg = '(?<=a href=")slides/.{1,25}\.pdf(?=">)'
+    cs61B reg = '(?<=a href=")materials/.{1,20}\.pdf(?=")'
 
 #main中的url
     #url = 'http://web.stanford.edu/class/cs161/schedule.html' #获取下载地址的url 需要分析
     #url = 'https://web.stanford.edu/class/archive/cs/cs166/cs166.1146/'
+    #url = 'http://www-inst.eecs.berkeley.edu/~cs61a/fa11/61a-python/content/www/index.html' cs61A
+    #url = 'https://computer-networks.github.io/sp19/lectures.html' 
+    #url = 'https://inst.eecs.berkeley.edu/~cs61b/fa19/' cs61A
+
+#CS234 reinforcement learning
+    url= r'http://web.stanford.edu/class/cs234/slides/'
+    reg = '(?<=a href=").{1,20}\.pdf(?=")'  
+
+# cmu 15-445
+    url = https://15445.courses.cs.cmu.edu/fall2019/schedule.html
+    reg ='(?<=href="\.).{1,30}\.pdf(?=")'
+    down_url = https://15445.courses.cs.cmu.edu/fall2019  +  /slides/23-distributedoltp.pdf
